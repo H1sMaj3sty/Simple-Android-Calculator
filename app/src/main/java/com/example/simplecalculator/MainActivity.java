d2b31190d056614de4a70c41c6350fcaaadd7a8a
Da Man <akuncadangannn2020@gmail.com>
2023-11-08 09:48:30 +0700

    Third Commit (Changed variables data type to double)

--- app/src/main/java/com/example/simplecalculator/MainActivity.java
+++ app/src/main/java/com/example/simplecalculator/MainActivity.java
@@ -20,52 +20,52 @@ public class MainActivity extends AppCompatActivity {
         bilangan1 = findViewById(R.id.bilangan1);
         bilangan2 = findViewById(R.id.bilangan2);
 
-        int result = 0;
     }
 
     public void tambah(View view) {
-        int angka1 = Integer.parseInt(bilangan1.getText().toString());
-        int angka2 = Integer.parseInt(bilangan2.getText().toString());
+        double angka1 = Integer.parseInt(bilangan1.getText().toString());
+        double angka2 = Integer.parseInt(bilangan2.getText().toString());
 
-        int result = angka1 + angka2;
+        double result = angka1 + angka2;
 
-        kirim(angka1, angka2, result, "+ (Add)");
+        kirim(angka1, angka2, result, 0, "+ (Add)");
     }
 
     public void kurang(View view) {
-        int angka1 = Integer.parseInt(bilangan1.getText().toString());
-        int angka2 = Integer.parseInt(bilangan2.getText().toString());
+        double angka1 = Integer.parseInt(bilangan1.getText().toString());
+        double angka2 = Integer.parseInt(bilangan2.getText().toString());
 
-        int result = angka1 - angka2;
+        double result = angka1 - angka2;
 
-        kirim(angka1, angka2, result, "- (Minus)");
+        kirim(angka1, angka2, result, 0, "- (Minus)");
     }
 
     public void kali(View view) {
-        int angka1 = Integer.parseInt(bilangan1.getText().toString());
-        int angka2 = Integer.parseInt(bilangan2.getText().toString());
+        double angka1 = Integer.parseInt(bilangan1.getText().toString());
+        double angka2 = Integer.parseInt(bilangan2.getText().toString());
 
-        int result = angka1 * angka2;
+        double result = angka1 * angka2;
 
-        kirim(angka1, angka2, result, "* (Multiply)");
+        kirim(angka1, angka2, result, 0, "* (Multiply)");
     }
 
     public void bagi(View view) {
-        int angka1 = Integer.parseInt(bilangan1.getText().toString());
-        int angka2 = Integer.parseInt(bilangan2.getText().toString());
+        double angka1 = Integer.parseInt(bilangan1.getText().toString());
+        double angka2 = Integer.parseInt(bilangan2.getText().toString());
+        double mod = angka1 % angka2;
 
-        int result = angka1 / angka2;
+        double result = angka1 / angka2;
 
-        kirim(angka1, angka2, result, "/ (Divide)");
+        kirim(angka1, angka2, result, mod,"/ (Divide)");
     }
 
-    public void kirim(int angka1, int angka2, int result, String operasi) {
+    public void kirim(double angka1, double angka2, double result, double mod, String operasi) {
         Intent intent = new Intent(this, SecondActivity.class);
         intent.putExtra("hasilOperasi", result);
         intent.putExtra("bilangan1", angka1);
         intent.putExtra("bilangan2", angka2);
+        intent.putExtra("modulus", mod);
         intent.putExtra("operasi", operasi);
-
 
         startActivity(intent);
     }
--- app/src/main/java/com/example/simplecalculator/SecondActivity.java
+++ app/src/main/java/com/example/simplecalculator/SecondActivity.java
@@ -19,15 +19,17 @@ public class SecondActivity extends AppCompatActivity{
         hasilbil = findViewById(R.id.hasil);
 
         Intent intent = getIntent();
-        if (intent.hasExtra("hasilOperasi") && intent.hasExtra("bilangan1") && intent.hasExtra("bilangan2") && intent.hasExtra("operasi")) {
-            int hasilOperasi = intent.getIntExtra("hasilOperasi", 0);
-            int bil1 = intent.getIntExtra("bilangan1", 0);
-            int bil2 = intent.getIntExtra("bilangan2", 0);
+        if (intent.hasExtra("hasilOperasi") && intent.hasExtra("bilangan1") && intent.hasExtra("bilangan2") && intent.hasExtra("modulus") && intent.hasExtra("operasi")) {
+            double hasilOperasi = intent.getDoubleExtra("hasilOperasi", 0);
+            double bil1 = intent.getDoubleExtra("bilangan1", 0);
+            double bil2 = intent.getDoubleExtra("bilangan2", 0);
+            double mod = intent.getDoubleExtra("modulus", 0);
             String operasi = intent.getStringExtra("operasi");
 
             String hasilText = "Bilangan 1\t\t:  " + bil1 + "\n" +
                     "Bilangan 2\t\t:  " + bil2 + "\n" +
                     "Operasi\t\t\t\t\t:  " + operasi + "\n" +
+                    "Modulus\t\t\t\t: " + mod + "\n" +
                     "Hasil\t\t\t\t\t\t\t\t:  " + hasilOperasi;
 
             hasilbil.setText(hasilText);
